26c83904f5de7157e23bf4ee46fbc64253bbb13f
i.2galadriel <toky.andrianina401@gmail.com>
2024-12-12 19:46:41 +0300

    Initial commit

---
+++ airport-frontoffice/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace airport_frontoffice.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
